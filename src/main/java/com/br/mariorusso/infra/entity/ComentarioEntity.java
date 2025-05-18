599634891b07ce6556fb710d1faeeb43167b9c96
Mário Russo <mario.russo93@hotmail.com>
2025-05-18 20:39:10 -0300

    Feat: entidade da classe comentário

---
+++ src/main/java/com/br/mariorusso/infra/entity/ComentarioEntity.java
@@ -0,0 +1,64 @@
+package com.br.mariorusso.infra.entity;
+
+import java.time.LocalDateTime;
+
+import com.br.mariorusso.core.model.Comentario;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "comentario")
+public class ComentarioEntity extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    private String conteudo;
+
+    @Column(nullable = false)
+    public LocalDateTime dataComentario;
+
+    @Column(nullable = false)
+    
+    @ManyToOne()
+    @JoinColumn(name = "usuario_id")
+    public UsuarioEntity usuario;
+
+    @Column(nullable = false)
+    @ManyToOne()
+    @JoinColumn(name = "publicacao_id")
+    public PublicacaoEntity publicacao;
+
+    public static ComentarioEntity fromDomain(Comentario comentario){
+        ComentarioEntity entity = new ComentarioEntity();
+
+        entity.conteudo = comentario.getConteudo();
+        entity.dataComentario = comentario.getDataComentario();
+        entity.id = comentario.getId();
+        entity.publicacao =PublicacaoEntity.fromDomain(comentario.getPublicacao());
+        entity.usuario = UsuarioEntity.fromDomain(comentario.getUsuario());
+
+        return entity;
+    }
+
+    public Comentario toDomain(){
+        Comentario comentario = new Comentario();
+
+        comentario.setConteudo(conteudo);
+        comentario.setDataComentario(dataComentario);
+        comentario.setId(id);
+        comentario.setPublicacao(publicacao.toDomain());
+        comentario.setUsuario(usuario.toDomain());
+         return comentario;
+    }
+
+}
