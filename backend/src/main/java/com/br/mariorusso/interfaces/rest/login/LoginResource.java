c685d4b5aac6e1c8ea7e10a14c6c571bc202dcff
Mário Russo <mario.russo93@hotmail.com>
2025-06-01 17:55:01 -0300

    Feat: tratamento do erro no endpoint login, caso o usuárionão seja encontrado

--- backend/src/main/java/com/br/mariorusso/interfaces/rest/login/LoginResource.java
+++ backend/src/main/java/com/br/mariorusso/interfaces/rest/login/LoginResource.java
@@ -1,5 +1,6 @@
 package com.br.mariorusso.interfaces.rest.login;
 
+import com.br.mariorusso.application.Login;
 import com.br.mariorusso.core.service.LoginCore;
 import com.br.mariorusso.infra.entity.UsuarioEntity;
 
@@ -11,17 +12,38 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.HashMap;
+
 @Path("/login")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class LoginResource {
 
+
+    final LoginCore<UsuarioEntity> loginUsuario;
+
     @Inject
-    LoginCore<UsuarioEntity> loginUsuario;
-    
+    public LoginResource(LoginCore<UsuarioEntity> loginUsuario) {
+        this.loginUsuario = loginUsuario;
+    }
+
     @POST
     public Response login(LoginDtoIn login){
-        UsuarioEntity login2 = loginUsuario.login(login.senha(), login.email());
-        return Response.ok( new LoginDtoOut(login2.email, login2.nome)).build();
+        try{
+            UsuarioEntity login2 = loginUsuario.login(login.senha(), login.email());
+            return Response.ok( new LoginDtoOut(login2.email, login2.nome)).build();
+
+        } catch (Exception e) {
+            return  Response.status(Response.Status.NOT_FOUND).entity("Usuário não encontrado").build();
+        }
+
+
+    }
+    @Path("/register")
+    @POST()
+    public  Response register (RegisterDto dto){
+        Login login = (Login) loginUsuario;
+        login.register(dto);
+        return  Response.ok("Usuário salvo com sucesso!!").build();
     }
 }
