897263cb5b9f96c59cc6195fa8617ce5d1b5693f
Mário Russo <mario.russo93@hotmail.com>
2025-06-01 18:00:47 -0300

    feat : criação do arquivo de teste

---
+++ backend/src/test/java/com/br/mariorusso/interfaces/rest/comentarios/ComentarioResourceTest.java
@@ -0,0 +1,20 @@
+package com.br.mariorusso.interfaces.rest.comentarios;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComentarioResourceTest {
+
+    @Test
+    void salvaComentrio() {
+    }
+
+    @Test
+    void buscaTodosComentarios() {
+    }
+
+    @Test
+    void buscacomentarioPorId() {
+    }
+}
